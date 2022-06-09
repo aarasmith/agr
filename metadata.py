b101303e50731990fe9d62c84e29eca033c68954
Andy Arasmith <andyarasmith@gmail.com>
2022-06-09 07:28:05 -0400

    2022/06/09 - Laptop - fixed comment tag, copy and rename vs move, changed dict elements to lists

--- metadata.py
+++ metadata.py
@@ -9,22 +9,26 @@ import uuid
 import os
 from dataclasses import dataclass
 from translate import GoogleTranslate as gt
+from shutil import copy2
 
 @dataclass
 class metadata:
     
     @classmethod
-    def write_metadata(cls, filename, filepath = os.getcwd() + "/", destination = os.getcwd() + "/"):
-        vid_meta = MP4(filepath + filename)
-        basename = os.path.splitext(os.path.basename(filename))[0]
+    def write_metadata(cls, filepath, destination = os.getcwd(), keep_original = True):
+        vid_meta = MP4(filepath)
+        basename = os.path.splitext(os.path.basename(filepath))[0]
         vid_meta['\xa9nam'] = basename
         translation = gt.translate(basename)
         name_en = basename
         if translation.language != "en":
-            vid_meta['xa9cmt'] = translation.translation
+            vid_meta['\xa9cmt'] = translation.translation
             name_en = translation.translation
         vid_meta.save()
         unique_name = uuid.uuid4().hex
-        os.rename(filepath + filename, destination + unique_name + ".mp4")
+        if keep_original:
+            copy2(filepath, os.path.join(destination, unique_name + ".mp4"))
+        else:
+            os.rename(filepath, os.path.join(destination, unique_name + ".mp4"))
         
-        return {"original_title": basename, "filename": unique_name, "title_en": name_en}
+        return {"original_title": [basename], "filename": [unique_name], "title_en": [name_en]}
