25dc043d98d37c04dbf6b77a0c832a4bc118600f
Andy Arasmith <andyarasmith@gmail.com>
2022-06-11 06:11:00 -0400

    2022/06/11 - Laptop - initial commit

---
+++ schema.sql
@@ -0,0 +1,34 @@
+CREATE TABLE videos (
+
+    file_name TEXT PRIMARY KEY NOT NULL,
+    
+    original_collection TEXT,
+    
+    original_title TEXT,
+    
+    title_en TEXT,
+    
+    year INT,
+    
+    date TEXT,
+    
+    armed_group TEXT,
+    
+    adm1 TEXT,
+    
+    adm2 TEXT,
+    
+    adm3 TEXT,
+    
+    adm4 TEXT,
+    
+    place_name TEXT,
+    
+    is_training BOOL,
+    
+    is_indexed BOOL,
+    
+    vi_id TEXT,
+    
+    insights TEXT
+);
