12695fa0fec8a2c0b9e75a21b1d3dd07072a7386
Andy Arasmith <andyarasmith@gmail.com>
2022-06-12 13:46:43 -0400

    2022/06/12 - Laptop - refactored syno_fs

--- syno_fs.py
+++ syno_fs.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-folder_path = "/Main/Visual/Video projects/Conflict Videos/Syria/Syria Addendum/Doc War Syria Addendum"
+#folder_path = "/Main/Visual/Video projects/Conflict Videos/Syria/Syria Addendum/Doc War Syria Addendum"
 @dataclass
 class syno_fs:
     
@@ -19,20 +19,16 @@ class syno_fs:
     _syno_port: str = field(default=os.environ['SYNO_PORT'], init=False, repr=False)
     _syno_user: str = field(default=os.environ['SYNO_USER'], init=False, repr=False)
     _syno_pass: str = field(default=os.environ['SYNO_PASS'], init=False, repr=False)
+    fs: object = field(init=False)        
     
-    @classmethod
-    def _init_fs(cls):
-        return filestation.FileStation(cls._syno_ip, cls._syno_port, cls._syno_user, cls._syno_pass, secure=False, cert_verify=False, dsm_version=6, debug=True, otp_code=None)
-    
-    @classmethod
-    def list_files(cls, folder_path):
-        fs = cls._init_fs()
-        return fs.get_file_list(folder_path, )
-    
-    @classmethod
-    def download_file(cls, file_path, dest_path):
-        fs = cls._init_fs()
-        fs.get_file(file_path, mode="download", dest_path = dest_path)
+    def __post_init__(self):
+        self.fs = filestation.FileStation(self._syno_ip, self._syno_port, self._syno_user, self._syno_pass, secure=False, cert_verify=False, dsm_version=6, debug=True, otp_code=None)
+        
+    def list_files(self, folder_path):
+        return self.fs.get_file_list(folder_path)
+        
+    def download_file(self, file_path, dest_path):
+        self.fs.get_file(file_path, mode="download", dest_path = dest_path)
         return(os.path.join(dest_path, os.path.basename(file_path)))
     
     # fl = filestation.FileStation(syno_ip, syno_port, syno_user, syno_pass, secure=False, cert_verify=False, dsm_version=6, debug=True, otp_code=None)
